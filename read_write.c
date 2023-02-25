aab61a15473d4e25913c67db52014f85a1d22b22
HLoVer <96529961+HLoVerK@users.noreply.github.com>
2023-02-25 16:34:58 +0600

    Update read_write.c

--- read_write.c
+++ read_write.c
@@ -4,107 +4,34 @@
 #include <linux/cdev.h>
 #include <linux/uaccess.h>
 
+/* Meta info*/
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Noyan");
-MODULE_DESCRIPTION("Imitation Device Callbacks - Linux Kernel Module");
+MODULE_AUTHOR("Sapargali");
+MODULE_DESCRIPTION("A simple LKM to demostrate the use of parameters");
 
-static char buffer[255];
-static int buffer_pointer = 0;
+/* Kernel Module's Parametrs */
+static unsigned int gpio_nr = 12;
+static char *device_name = "Sapar";
 
-static dev_t my_device_nr;
-static struct class *my_class;
-static struct cdev my_device;
+module_param(gpio_nr, uint, S_TRUGO);
+module_param(device_name, charp, S_TRUGO);
 
-#define DRIVER_NAME "dummydriver"
-#define DRIVER_CLASS "MyModuleClass"
+MODULE_PARM_DESC(gpio_nr, "Nr. of GPIO to use in this LKM");
+MODULE_PARAM_DESC(device_name, "Device name to use in this LKM");
 
-static ssize_t driver_read(struct file *File, char *user_buffer, size_t count, loff_t *offs) {
-	int to_copy, not_copied, delta;
-
-	to_copy = min(count, buffer_pointer);
-
-	not_copied = copy_to_user(user_buffer, buffer, to_copy);
-
-	delta = to_copy - not_copied;
-
-	return delta;
-}
-
-static ssize_t driver_write(struct file *File, const char *user_buffer, size_t count, loff_t *offs) {
-	int to_copy, not_copied, delta;
-
-	to_copy = min(count, sizeof(buffer));
-
-	not_copied = copy_from_user(buffer, user_buffer, to_copy);
-	buffer_pointer = to_copy;
-
-	delta = to_copy - not_copied;
-
-	return delta;
-}
-
-static int driver_open(struct inode *device_file, struct file *instance) {
-	printk("dev_nr - open was called!\n");
-	return 0;
-}
-
-static int driver_close(struct inode *device_file, struct file *instance) {
-	printk("dev_nr - close was called!\n");
-	return 0;
-}
-
-static struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.open = driver_open,
-	.release = driver_close,
-	.read = driver_read,
-	.write = driver_write
-};
-
+/*
+  This function is called, when the module is loaded into the kernel
+*/
 static int __init ModuleInit(void) {
-	int retval;
-	printk("Hello, Kernel!\n");
-
-	if( alloc_chrdev_region(&my_device_nr, 0, 1, DRIVER_NAME) < 0) {
-		printk("Device Nr. could not be allocated!\n");
-		return -1;
-	}
-	printk("read_write - Device Nr. Major: %d, Minor: %d was registered!\n", my_device_nr >> 20, my_device_nr && 0xfffff);
-
-	if((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
-		printk("Device class can not be created!\n");
-		goto ClassError;
-	}
-
-	if(device_create(my_class, NULL, my_device_nr, NULL, DRIVER_NAME) == NULL) {
-		printk("Can not create device file!\n");
-		goto FileError;
-	}
-
-	cdev_init(&my_device, &fops);
-
-	if(cdev_add(&my_device, my_device_nr, 1) == -1) {
-		printk("Registering of device to kernel failed!\n");
-		goto AddError;
-	}
-
-	return 0;
-AddError:
-	device_destroy(my_class, my_device_nr);
-FileError:
-	class_destroy(my_class);
-ClassError:
-	unregister_chrdev_region(my_device_nr, 1);
-	return -1;
+  printk("gpio_nr = %u\n", gpio_nr);
+  printk("device_name = %s\n", device_name);
+  printk("hello, new Kernel!\n");
+  return 0;
 }
 
+/*
+  This function is called, when the module is removed from the kernel
+*/
 static void __exit ModuleExit(void) {
-	cdev_del(&my_device);
-	device_destroy(my_class, my_device_nr);
-	class_destroy(my_class);
-	unregister_chrdev_region(my_device_nr, 1);
-	printk("Goodbye, Kernel\n");
+  printk("Goodbye, Kernel\n");
 }
-
-module_init(ModuleInit);
-module_exit(ModuleExit);
